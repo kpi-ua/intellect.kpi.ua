1c291bc4f3bf9430f3f4c86843a782ca3a998d6d
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-12-01 16:52:09 +0200

    Init angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name intellectApp
+ * @description
+ * # intellectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('intellectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intellectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the intellectApp
+ */
+angular.module('intellectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
