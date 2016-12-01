61723f26e812bbfa1d326c75e4af68933fd346cf
KBIS <kbis_kpi@ukr.net>
2016-12-01 17:29:59 +0200

    Files from legacy project added (will be removed in future)

---
+++ app/scripts/legacy/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Core.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class HomeController : Core.Controller
+    {
+        [OutputCache(Duration = OutputCacheDuration, VaryByParam = "*")]
+        public ActionResult Index()
+        {
+            var model = new HomeViewModel
+            {
+                Publication = _client.GetLastPublication(),
+                Slides = GetSlides()
+            };
+
+            return View(model);
+        }
+
+        private IEnumerable<string> GetSlides()
+        {
+            var path = Server.MapPath("~/static/slides");
+            var files = Directory.GetFiles(path);
+            return files.Select(o => "/static/slides/" + Path.GetFileName(o)).ToList();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = OutputCacheDuration, VaryByParam = "*")]
+        public new ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ app/scripts/legacy/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class PageController : Core.Controller
+    {
+        [OutputCache(Duration = OutputCacheDuration, VaryByParam = "*")]
+        public ActionResult Index(string name)
+        {
+            var html = LoadPage(name);
+
+            if (String.IsNullOrEmpty(html))
+            {
+                return HttpNotFound();
+            }
+
+            return View((Object)html);
+        }
+
+    }
+}
