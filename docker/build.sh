45ef3b93ef3723e1d347de751914edebd2ecca73
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2020-03-01 14:47:35 +0200

    Update build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-project='intellect-kpi-ua'
-now=`date +%Y%m%d%H%M%S`
+project="intellect-kpi-ua"
+now=$(date +%Y%m%d%H%M%S)
 root_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)/../
 
-github_organization='docker.pkg.github.com/kpi-ua/intellect.kpi.ua'
-github_registry_url='docker.pkg.github.com'
+github_organization="docker.pkg.github.com/kpi-ua/intellect.kpi.ua"
+github_registry_url="docker.pkg.github.com"
 github_registry_username=$1
 github_registry_pwd=$2
 
-dockerhub_organization='kpiua'
-dockerhub_registry_url='registry-1.docker.io'
+dockerhub_organization="kpiua"
+dockerhub_registry_url=""
 dockerhub_registry_username=$3
 dockerhub_registry_pwd=$4
 
@@ -18,27 +18,27 @@ clear
 
 push_to_docker_registry()  {
     local organization=$1
-    local project=$2
-    local tag=$3
-    local registry_url=$4
-    local registry_username=$5
-    local registry_pwd=$6
+    local image_id=$2:$3
+    local username=$4
+    local password=$5
+    local server=$6
 
-    echo "Docker Auth ${registry_url}"
-    
-    docker login -u $registry_username -p $registry_pwd $registry_url
+    echo "Docker Auth ${server}"
 
-    echo "Push Docker images ${registry_url}"
+    docker login -u "$username" -p "$password" "$server"
 
-    docker tag $project:$tag $organization/$project:$tag
-    docker push $organization/$project:$tag
+    echo "Push Docker images ${server}"
+
+    docker tag "$image_id" "$organization"/"$image_id"
+    docker push "$organization"/"$image_id"
+    docker logout
 }
 
 #################################################################################
 
 echo "Build project"
 
-cd $root_dir
+cd "$root_dir" || exit
 
 dotnet build ./src/Site/Site.csproj --configuration Release
 dotnet publish ./src/Site/Site.csproj --configuration Release -o ./docker/out
@@ -46,17 +46,18 @@ rm ./docker/out/appsettings.Development.json
 
 #################################################################################
 
-cd $root_dir/docker
+cd "$root_dir"/docker || exit
 
 echo "Build docker images"
 
-docker build ./ --file ./.dockerfile --tag $project:$now --tag $project:latest
+docker build ./ --file ./.dockerfile --tag "$project":"$now"
+docker build ./ --file ./.dockerfile --tag "$project":latest
 
 echo "Publish docker images"
 
-push_to_docker_registry $github_organization $project $now $github_registry_url $github_registry_username $github_registry_pwd
-push_to_docker_registry $github_organization $project latest $github_registry_url $github_registry_username $github_registry_pwd
-push_to_docker_registry $dockerhub_organization $project $now $dockerhub_registry_url $dockerhub_registry_username $dockerhub_registry_pwd
-push_to_docker_registry $dockerhub_organization $project latest $dockerhub_registry_url $dockerhub_registry_username $dockerhub_registry_pwd
+push_to_docker_registry $github_organization "$project" "$now" "$github_registry_username" "$github_registry_pwd" "$github_registry_url"
+push_to_docker_registry $github_organization "$project" latest "$github_registry_username" "$github_registry_pwd" "$github_registry_url"
+push_to_docker_registry $dockerhub_organization "$project" "$now" "$dockerhub_registry_username" "$dockerhub_registry_pwd" "$dockerhub_registry_url"
+push_to_docker_registry $dockerhub_organization "$project" latest "$dockerhub_registry_username" "$dockerhub_registry_pwd" "$dockerhub_registry_url"
 
 echo "Done"
