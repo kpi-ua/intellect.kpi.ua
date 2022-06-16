f93dd6f9e798e3c77283802419dda21666f1f601
Andrew Gubskiy <ernado-x@users.noreply.github.com>
2022-06-16 20:59:01 +0300

    Update profile links, update packages (#38)
    * Update links rendering
    * Update contact helper
    * Update Profile.js
    * Add address link
    * Update CSS
    * Format code
    * Detect social links as website
    * Update packages

--- src/src/components/Profile/Profile.js
+++ src/src/components/Profile/Profile.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Profile.css';
 import {
-  withRouter,
-  matchPath
+    withRouter,
+    matchPath
 } from "react-router-dom";
 import api from "../../services/api";
 import {Button, OverlayTrigger, Tooltip} from "react-bootstrap";
@@ -11,206 +11,208 @@ import ProfileAvatar from "../ProfileAvatar";
 
 class Profile extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      profile: {}
-    };
-  }
-
-  async componentDidMount(){
-    const profileId = this.getProfileId();
-    const profile = await api.getProfile(profileId);
-
-    this.setState({profile});
-
-  }
-
-  getProfileId() {
-    const match = matchPath(this.props.history.location.pathname, {
-      path: '/profile/:profileId',
-      exact: true,
-      strict: false
-    });
-
-    return  match.params.profileId
-  }
-
-  render() {
-
-    const profile = this.state.profile;
-
-    function generalInformationExist(profile) {
-      return  !!profile && (
-        !!profile.academicDegree  ||
-        !!profile.academicStatus  ||
-        !!profile.scientificInterest);
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: {}
+        };
     }
 
-    function employerInformationExist(profile) {
-      return !!profile && !!profile.positions && profile.positions.length > 0;
+    async componentDidMount() {
+        const profileId = this.getProfileId();
+        const profile = await api.getProfile(profileId);
+
+        this.setState({profile});
+
     }
 
-    function renderTooltip(value, tooltip) {
-      return <OverlayTrigger
-        placement="right"
-        delay={{ show: 250, hide: 400 }}
-        overlay={(props) => {
-          return  <Tooltip {...props}> {tooltip} </Tooltip>
-        }}
-      >
-        <span className="label-with-tooltip">{value}</span>
-      </OverlayTrigger>
+    getProfileId() {
+        const match = matchPath(this.props.history.location.pathname, {
+            path: '/profile/:profileId',
+            exact: true,
+            strict: false
+        });
+
+        return match.params.profileId
     }
 
-    function renderPositionOrDegree(name) {
-      name = !name ? '' : name.trim();
+    render() {
 
-      if (name === "Доцент") {
-        return renderTooltip(name, "В Україні та інших країнах вчене звання викладачів закладів вищої освіти, що виконують функцію університетських лекторів; вчене звання співробітників наукових установ; посада в закладах вищої освіти. Учене звання доцента присвоюється ученими радами. Звання доцента засвідчується атестатом, що видає Міністерство освіти України. ")
-      }
+        const profile = this.state.profile;
 
-      if (name === "Професор") {
-        return renderTooltip(name, "Вчене звання і посада викладача закладу вищої освіти чи наукового співробітника науково-дослідної установи.")
-      }
+        function generalInformationExist(profile) {
+            return !!profile && (
+                !!profile.academicDegree ||
+                !!profile.academicStatus ||
+                !!profile.scientificInterest);
+        }
 
-      if (name === "Науковий співробітник") {
-        return renderTooltip(name, "Вчене звання старшого наукового співробітника в Україні присвоювалося докторам і кандидатам наук із стажем наукової роботи не менше трьох років, які працювали у вищих навчальних закладах III—IV рівня акредитації або наукових установах та організаціях до них прирівняних і зараховані після обрання за конкурсом чи в порядку атестації.")
-      }
+        function employerInformationExist(profile) {
+            return !!profile && !!profile.positions && profile.positions.length > 0;
+        }
 
-      if (name === "Кандидат наук") {
-        return renderTooltip(name, "Науковий ступінь кандидата наук присуджується спеціалізованою вченою радою на підставі прилюдного захисту дисертації та затверджується Міністерством освіти і науки України з урахуванням висновку відповідної експертної ради.  Прирівнюється до ступеня доктора філософії.")
-      }
+        function renderTooltip(value, tooltip) {
+            return <OverlayTrigger
+                placement="right"
+                delay={{show: 250, hide: 400}}
+                overlay={(props) => {
+                    return <Tooltip {...props}> {tooltip} </Tooltip>
+                }}
+            >
+                <span className="label-with-tooltip">{value}</span>
+            </OverlayTrigger>
+        }
 
-      if (name === "Викладач Старший") {
-        return renderTooltip(name, "Викладацька посада у закладах вищої освіти, що займає проміжне положення між асистентом і доцентом. Старші викладачі можуть самостійно читати курси лекцій та приймати заліки та іспити.")
-      }
+        function renderPositionOrDegree(name) {
+            name = !name ? '' : name.trim();
 
-      return name
-    }
+            name = name.replaceAll("NULL", "").trim();
 
-    function renderPositions(profile) {
+            if (name === "Доцент") {
+                return renderTooltip(name, "В Україні та інших країнах вчене звання викладачів закладів вищої освіти, що виконують функцію університетських лекторів; вчене звання співробітників наукових установ; посада в закладах вищої освіти. Учене звання доцента присвоюється ученими радами. Звання доцента засвідчується атестатом, що видає Міністерство освіти України. ")
+            }
 
-      if (employerInformationExist(profile)) {
+            if (name === "Професор") {
+                return renderTooltip(name, "Вчене звання і посада викладача закладу вищої освіти чи наукового співробітника науково-дослідної установи.")
+            }
 
-        return profile.positions.map((p, index) =>
-          <div className="row" key={"position-" + index}>
-            <div className="col-md-5 title">
-              <a target="_subdivision" href={p.subdivision.url}>{p.subdivision.name}</a>
-            </div>
-            <div className="col-md-7">{renderPositionOrDegree(p.name)}</div>
-          </div>
-        );
-      }
+            if (name === "Науковий співробітник") {
+                return renderTooltip(name, "Вчене звання старшого наукового співробітника в Україні присвоювалося докторам і кандидатам наук із стажем наукової роботи не менше трьох років, які працювали у вищих навчальних закладах III—IV рівня акредитації або наукових установах та організаціях до них прирівняних і зараховані після обрання за конкурсом чи в порядку атестації.")
+            }
 
-      return null;
-    }
+            if (name === "Кандидат наук") {
+                return renderTooltip(name, "Науковий ступінь кандидата наук присуджується спеціалізованою вченою радою на підставі прилюдного захисту дисертації та затверджується Міністерством освіти і науки України з урахуванням висновку відповідної експертної ради.  Прирівнюється до ступеня доктора філософії.")
+            }
 
-    return (
+            if (name === "Викладач Старший") {
+                return renderTooltip(name, "Викладацька посада у закладах вищої освіти, що займає проміжне положення між асистентом і доцентом. Старші викладачі можуть самостійно читати курси лекцій та приймати заліки та іспити.")
+            }
 
-        <div className="row">
-          <div className="col-md-3">
-            <ProfileAvatar profile={this.state.profile} hideBackLink={true} />
-          </div>
+            return name
+        }
 
-          <div className="col-md-9 profile-information">
-            <h1>{profile.fullName}</h1>
+        function renderPositions(profile) {
 
-            {!!profile.credo && <h5 className="credo">{profile.credo}</h5>}
+            if (employerInformationExist(profile)) {
 
-            { generalInformationExist(profile) &&
-              <section>
-                <h4>Загальна інформація</h4>
+                return profile.positions.map((p, index) =>
+                    <div className="row" key={"position-" + index}>
+                        <div className="col-md-5 title">
+                            <a target="_subdivision" href={p.subdivision.url}>{p.subdivision.name}</a>
+                        </div>
+                        <div className="col-md-7">{renderPositionOrDegree(p.name)}</div>
+                    </div>
+                );
+            }
 
-                {
-                  !!profile.academicDegree &&
-                  <div className="row">
-                    <div className="col-md-5 title">Науковий ступень</div>
-                    <div className="col-md-7">{renderPositionOrDegree(profile.academicDegree)}</div>
-                  </div>
-                }
-                {
-                  !!profile.academicStatus &&
-                  <div className="row">
-                    <div className="col-md-5 title">Вчене звання</div>
-                    <div className="col-md-7">{renderPositionOrDegree(profile.academicStatus)}</div>
-                  </div>
-                }
-                {
-                  !!profile.scientificInterest &&
-                  <div className="row">
-                    <div className="col-md-5 title">Наукові інтереси</div>
-                    <div className="col-md-7">{profile.scientificInterest}</div>
-                  </div>
-                }
+            return null;
+        }
 
-                <div className="row">
-                  <div className="col-md-5 title">Адреса публічної сторінки</div>
-                  <div className="col-md-7">
-                    <a href={"https://intellect.kpi.ua/profile/" + profile.userIdentifier}>
-                      {"intellect.kpi.ua/profile/" + profile.userIdentifier}
-                    </a>
-                  </div>
+        return (
+
+            <div className="row">
+                <div className="col-md-3">
+                    <ProfileAvatar profile={this.state.profile} hideBackLink={true}/>
                 </div>
 
-              </section>
-            }
+                <div className="col-md-9 profile-information">
+                    <h1>{profile.fullName}</h1>
 
-            { employerInformationExist(profile) &&
-              <section>
-                <h4>Дані за місцем роботи</h4>
-                {renderPositions(profile)}
-              </section>
-            }
+                    {!!profile.credo && <h5 className="credo">{profile.credo}</h5>}
 
-            <section>
-              <h4>Наукова діяльність</h4>
+                    {generalInformationExist(profile) &&
+                        <section>
+                            <h4>Загальна інформація</h4>
 
-              <Button
-                size="lg" block
-                variant="outline-success"
-                href={'/profile/' + this.state.profile.userIdentifier + '/publications'}>
-                Публікації
-                &nbsp;
-                <Icon.Newspaper />
-              </Button>
+                            {
+                                !!profile.academicDegree &&
+                                <div className="row">
+                                    <div className="col-md-5 title">Науковий ступень</div>
+                                    <div className="col-md-7">{renderPositionOrDegree(profile.academicDegree)}</div>
+                                </div>
+                            }
+                            {
+                                !!profile.academicStatus &&
+                                <div className="row">
+                                    <div className="col-md-5 title">Вчене звання</div>
+                                    <div className="col-md-7">{renderPositionOrDegree(profile.academicStatus)}</div>
+                                </div>
+                            }
+                            {
+                                !!profile.scientificInterest &&
+                                <div className="row">
+                                    <div className="col-md-5 title">Наукові інтереси</div>
+                                    <div className="col-md-7">{profile.scientificInterest}</div>
+                                </div>
+                            }
 
-              <Button
-                size="lg" block
-                variant="outline-success"
-                href={'/profile/' + this.state.profile.userIdentifier + '/executions'}>
-                Виконання науково-дослідних та дослідно-конструкторських робіт
-                &nbsp;
-                <Icon.XDiamond />
-              </Button>
+                            <div className="row">
+                                <div className="col-md-5 title">Адреса публічної сторінки</div>
+                                <div className="col-md-7">
+                                    <a href={"https://intellect.kpi.ua/profile/" + profile.userIdentifier}>
+                                        {"intellect.kpi.ua/profile/" + profile.userIdentifier}
+                                    </a>
+                                </div>
+                            </div>
 
-              <Button
-                size="lg" block
-                variant="outline-success"
-                href={'/profile/' + this.state.profile.userIdentifier + '/results'}>
-                Результати виконання науково-дослідних та дослідно-конструкторських робіт
-                &nbsp;
-                <Icon.XDiamondFill />
-              </Button>
+                        </section>
+                    }
 
-              <Button
-                size="lg" block
-                variant="outline-success"
-                href={'/profile/' + this.state.profile.userIdentifier + '/conference'}>
-                Конференції, виставки
-                &nbsp;
-                <Icon.People />
-              </Button>
-            </section>
+                    {employerInformationExist(profile) &&
+                        <section>
+                            <h4>Дані за місцем роботи</h4>
+                            {renderPositions(profile)}
+                        </section>
+                    }
 
-            <br />
-            <br />
-            <br />
+                    <section>
+                        <h4>Наукова діяльність</h4>
 
-          </div>
-        </div>
-    );
-  }
+                        <Button
+                            size="lg" block
+                            variant="outline-success"
+                            href={'/profile/' + this.state.profile.userIdentifier + '/publications'}>
+                            Публікації
+                            &nbsp;
+                            <Icon.Newspaper/>
+                        </Button>
+
+                        <Button
+                            size="lg" block
+                            variant="outline-success"
+                            href={'/profile/' + this.state.profile.userIdentifier + '/executions'}>
+                            Виконання науково-дослідних та дослідно-конструкторських робіт
+                            &nbsp;
+                            <Icon.XDiamond/>
+                        </Button>
+
+                        <Button
+                            size="lg" block
+                            variant="outline-success"
+                            href={'/profile/' + this.state.profile.userIdentifier + '/results'}>
+                            Результати виконання науково-дослідних та дослідно-конструкторських робіт
+                            &nbsp;
+                            <Icon.XDiamondFill/>
+                        </Button>
+
+                        <Button
+                            size="lg" block
+                            variant="outline-success"
+                            href={'/profile/' + this.state.profile.userIdentifier + '/conference'}>
+                            Конференції, виставки
+                            &nbsp;
+                            <Icon.People/>
+                        </Button>
+                    </section>
+
+                    <br/>
+                    <br/>
+                    <br/>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withRouter(Profile);
--- src/src/components/ProfileAvatar/ProfileAvatar.js
+++ src/src/components/ProfileAvatar/ProfileAvatar.js
@@ -1,102 +1,149 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './ProfileAvatar.css';
 import {Button} from "react-bootstrap";
 import * as Icon from 'react-bootstrap-icons';
+import ContactHelper from "../../services/contactHelper";
 
 class ProfileAvatar extends Component {
 
-  render() {
-    const profile = this.props.profile;
+    render() {
+        const profile = this.props.profile;
 
-    function renderContactBlock(title, value, index) {
+        function renderContactBlock(title, value, index) {
 
-      return <div className="row" key={"cr-" + index}>
-              <div className="col-2">
-                {title}
-              </div>
-              <div className="col-10">
-                {value}
-              </div>
+            return <div className="row" key={"cr-" + index}>
+                <div className="col-2">
+                    {title}
+                </div>
+                <div className="col-10">
+                    {value}
+                </div>
             </div>;
-    }
-
-    function renderContact(c, index) {
-
-      if (!c || !c.name || !c.value || c.value.trim() === '') {
-        return;
-      }
-
-      debugger;
-
-      if (c.name.toLowerCase().includes("e-mail")) {
-
-        return renderContactBlock(
-          <Icon.Envelope />,
-          <a href={"mailto:" + c.value}>{c.value}</a>,
-          index
-        )
-      }
-
-      if (c.name.toLowerCase().includes("телефон")) {
-        return renderContactBlock(
-          <Icon.Phone />,
-          <a href={"tel:" + c.value}>{c.value}</a>,
-          index
-        )
-      }
-
-      if (c.name.toLowerCase().includes("skype")) {
-        return renderContactBlock(
-          <Icon.Phone />,
-          <a href={"skype:" + c.value}>{c.value}</a>,
-          index
-        )
-      }
-
-      if (c.name.toLowerCase().includes("сайт")) {
-        return renderContactBlock(
-          <Icon.Window />,
-          <a target="_website" href={c.value}>{c.value.replace('https://', '').replace('http://', '')}</a>,
-          index
-        )
-      }
-
-      return renderContactBlock(
-        '',
-        c.value,
-        index
-      )
-    }
-
-    return (
-      !!profile &&
-      <div>
-        <img className="img-thumbnail" src={profile.photo} alt={profile.fullName} />
-
-        <div className="container contacts">
-          {
-            !!profile.contactRecords &&  profile.contactRecords.map((c, index) =>
-              renderContact(c, index)
-            )
-          }
-        </div>
-
-        <br />
-
-        {
-          !this.props.hideBackLink &&
-
-          <Button
-            size="lg" block
-            variant="outline-success"
-            href={'/profile/' + profile.userIdentifier}>
-            Повернутися до профілю
-            {/*<Icon.ArrowBarLeft />*/}
-          </Button>
         }
-      </div>
-    );
-  }
+
+        function renderContact(c, index) {
+
+            let contact = new ContactHelper();
+
+            if (contact.isEmpty(c)) {
+                return;
+            }
+
+            if (contact.isEmail(c)) {
+
+                return renderContactBlock(
+                    <Icon.Envelope/>,
+                    <a target="_blank" href={contact.renderEmailLink(c)}>{c.value}</a>,
+                    index
+                )
+            }
+
+            if (contact.isPhone(c)) {
+                return renderContactBlock(
+                    <Icon.Phone/>,
+                    <a target="_blank" href={contact.renderPhoneLink(c)}>{c.value}</a>,
+                    index
+                )
+            }
+
+            if (contact.isSkype(c)) {
+                return renderContactBlock(
+                    <Icon.Phone/>,
+                    <a target="_blank" href={contact.renderSkypeLink(c)}>{c.value}</a>,
+                    index
+                )
+            }
+
+            if (contact.isWebSite(c)) {
+                return renderContactBlock(
+                    <Icon.Window/>,
+                    <a target="_blank" href={contact.renderWebSiteLink(c)}>{contact.renderWebSiteTitle(c)}</a>,
+                    index
+                )
+            }
+
+            if (contact.isTelegram(c)) {
+                return renderContactBlock(
+                    <Icon.Telegram/>,
+                    <a target="_blank" href={contact.renderTelegramLink(c)}>{c.value}</a>,
+                    index
+                )
+            }
+
+            if (contact.isOrcidId(c)) {
+                return renderContactBlock(
+                    <Icon.Book/>,
+                    <a target="_blank" href={contact.renderOrcidIdLink(c)}>Orcid</a>,
+                    index
+                )
+            }
+
+            if (contact.isAddress(c)) {
+                return renderContactBlock(
+                    <Icon.Map/>,
+                    <a target="_blank" href={contact.renderAddress(c)}>{c.value}</a>,
+                    index
+                )
+            }
+
+            if (contact.isResearchId(c)) {
+                return renderContactBlock(
+                    <Icon.Book/>,
+                    <a target="_blank" href={contact.renderResearchIdLink(c)}>Research</a>,
+                    index
+                )
+            }
+
+            if (contact.isScopusId(c)) {
+                return renderContactBlock(
+                    <Icon.Book/>,
+                    <a target="_blank" href={contact.renderScopusIdLink(c)}>Scopus</a>,
+                    index
+                )
+            }
+
+            if (contact.isGoogleScholarId(c)) {
+                return renderContactBlock(
+                    <Icon.Book/>,
+                    <a target="_blank" href={contact.renderGoogleScholarIdLink(c)}>Google Scholar</a>,
+                    index
+                )
+            }
+
+            return renderContactBlock('', c.value, index)
+        }
+
+        return (
+            !!profile &&
+            <div>
+                <img className="img-thumbnail" src={profile.photo} alt={profile.fullName}/>
+
+                <div className="container contacts">
+                    {
+                        !!profile.contactRecords && profile.contactRecords
+                            //.sort((a, b) => a.value > b.value ? -1 : 1)
+                            .map((c, index) =>
+                                renderContact(c, index)
+                            )
+                    }
+                </div>
+
+                <br/>
+
+                {
+                    !this.props.hideBackLink &&
+
+                    <Button
+                        size="lg" block
+                        variant="outline-success"
+                        href={'/profile/' + profile.userIdentifier}>
+                        Повернутися до профілю
+                        {/*<Icon.ArrowBarLeft />*/}
+                    </Button>
+                }
+            </div>
+        );
+    }
 }
 
 export default ProfileAvatar;
