700c72bde5eb4786be8a0299336cb302db245e09
Andrew <andrew.gubskiy@outlook.com>
2023-12-03 00:48:42 +0200

    Fix title panel

--- src/components/JobLabel/JobLabel.tsx
+++ src/components/JobLabel/JobLabel.tsx
@@ -8,7 +8,7 @@ type Props = {
 const JobLabel: React.FC<Props> = ({ qualification, workplace }) => {
     return (
         <div className="inline-block">
-            <div className="flex text-white bg-primary rounded-lg p-0.5 pl-2 flex gap-2 items-center text-xs">
+            <div className="flex text-white bg-primary rounded-lg p-0.5 pl-2 flex gap-2 items-center whitespace-nowrap text-xs">
                 {qualification}
                 <div className="text-primary rounded-md p-1 bg-white w-full">{workplace}</div>
             </div>
