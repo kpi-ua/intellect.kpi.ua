66ade1c0ab4fe181eade4f9c3780120b27f5a3eb
Oleks-Y <borith1111@gmail.com>
2023-12-02 13:36:00 +0100

    fix: route paths in tailwind ocnfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+        // Or if using `src` directory:
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
     theme: {
         screens: {
             xs: '450px',
