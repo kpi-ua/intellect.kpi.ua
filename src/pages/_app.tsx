d7a24b452482ca27aac21f2f1f9c62db967f2e3a
Oleks-Y <borith1111@gmail.com>
2023-12-03 21:31:05 +0100

    fix: move fonts to _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,6 @@ import '../styles/global.css';
 import Layout from './layout';
 import { NextPage } from 'next';
 
-import Head from 'next/head';
-
 export type NextPageWithLayout<P = any, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -22,13 +20,6 @@ export default function Intellect({ Component, pageProps }: AppPropsWithLayout) 
 
     return (
         <>
-            <Head>
-                <title>Intellect</title>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
             <main>{getLayout(<Component {...pageProps} />)};</main>
         </>
     );
