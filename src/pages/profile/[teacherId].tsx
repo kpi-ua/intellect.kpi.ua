75f24c94beb510951b9a126bdd8ca1c861237bea
Andrew <andrew.gubskiy@outlook.com>
2023-12-11 11:10:07 +0200

    Fix API_BASE_URL

--- src/pages/profile/[teacherId].tsx
+++ src/pages/profile/[teacherId].tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { API_BASE_URL } from '../constants';
+import { API_BASE_URL } from '../../constants';
 
 import { getExperienceByTeacherId, getTeacherByTeacherId } from '@/api/teacher';
 
