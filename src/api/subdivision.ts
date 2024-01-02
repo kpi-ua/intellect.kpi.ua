b35c7a367376134d83402fd748ab50f47e0bd803
Andrew <andrew.gubskiy@outlook.com>
2024-01-02 16:50:17 +0200

    Cleanup search methods

--- src/api/subdivision.ts
+++ src/api/subdivision.ts
@@ -2,15 +2,10 @@ import Http, { API_BASE_URL } from './index';
 
 export const getFaculties = (): Promise<{ name: string }[]> => {
     return Http.get(API_BASE_URL + '/subdivision/faculty/')
-        .then((response: any) => {
-            return response.map((faculty: { name: string; }) => faculty.name);
-        });
-
+        .then((response: any) => response.map((faculty: { name: string; }) => faculty.name));
 };
 
 export const getByQueryString = (q: string): Promise<string[]> => {
     return Http.get(API_BASE_URL + '/subdivision?q=' + q)
-        .then((response: any) => {
-            return response.map((department: { name: string; }) => department.name);
-        });
+        .then((response: any) => response.map((department: { name: string; }) => department.name));
 };
