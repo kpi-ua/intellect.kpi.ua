d3c3f13e71e3ac2a1515f6044babade87a9f243f
Andrew <andrew.gubskiy@outlook.com>
2024-01-02 16:41:08 +0200

    Fix search methods

--- src/api/subdivision.ts
+++ src/api/subdivision.ts
@@ -1,9 +1,16 @@
 import Http, { API_BASE_URL } from './index';
 
 export const getFaculties = (): Promise<{ name: string }[]> => {
-    return Http.get(API_BASE_URL + '/subdivision/faculty/');
+    return Http.get(API_BASE_URL + '/subdivision/faculty/')
+        .then((response: any) => {
+            return response.map((faculty: { name: string; }) => faculty.name);
+        });
+
 };
 
 export const getByQueryString = (q: string): Promise<string[]> => {
-    return Http.get('/subdivision?q=' + q);
+    return Http.get(API_BASE_URL + '/subdivision?q=' + q)
+        .then((response: any) => {
+            return response.map((department: { name: string; }) => department.name);
+        });
 };
