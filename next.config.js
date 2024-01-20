1dc6bda6313d556191ed84f53dadb53912ecca3d
Oleks-Y <borith1111@gmail.com>
2024-01-20 16:49:50 +0100

    feat: add sitemap

--- next.config.js
+++ next.config.js
@@ -8,6 +8,23 @@ const nextConfig = {
             },
         ],
     },
+    redirects: async () => {
+        return [
+            {
+                source: '/api/:route*',
+                destination: '/:route*',
+                permanent: true,
+            },
+        ];
+    },
+    rewrites: async () => {
+        return [
+            {
+                source: '/intellect/v2/sitemap',
+                destination: 'https://api.campus.kpi.ua/intellect/v2/sitemap',
+            },
+        ];
+    }
 };
 
 module.exports = nextConfig;
