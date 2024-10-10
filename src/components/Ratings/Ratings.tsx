6456138ed24edfec2c3c63c2ce98e12ac070cf2c
Andrew <andrew.gubskiy@outlook.com>
2024-10-10 15:51:32 +0300

    Add description for rating

--- src/components/Ratings/Ratings.tsx
+++ src/components/Ratings/Ratings.tsx
@@ -6,31 +6,38 @@ interface RatingsProps {
 
 export const Ratings = ({ ratings }: RatingsProps) => {
     return (
-        <div className="w-full overflow-x-scroll">
-            <table className="-ml-6 -mr-6 border-separate whitespace-nowrap border-spacing-x-6 border-spacing-y-2">
-                <thead className="text-sm text-left text-primary">
-                    <tr className="border-2 border-solid border-neutral-950">
-                        <th>Навчальний рік</th>
-                        <th>Рейтинг з навчально-методичної роботи</th>
-                        <th>Рейтинг з науково-інноваційної роботи</th>
-                        <th>Рейтинг з організаційно-виховної роботи</th>
-                        <th>Загальний рейтинг</th>
-                        <th>Кафедра</th>
-                    </tr>
-                </thead>
-                <tbody className="text-xs text-neutral-600">
-                    {ratings.map(rating => (
-                        <tr key={rating.studyYear}>
-                            <td>{rating.studyYear}</td>
-                            <td>{rating.educationalMethodologicalRating}</td>
-                            <td>{rating.scientificInnovativeRating}</td>
-                            <td>{rating.organizationalEducationalRating}</td>
-                            <td>{rating.overallRating}</td>
-                            <td>{rating.subdivision.name}</td>
+        <div className="relative justify-between gap-24 mt-4">
+            <p className="text-xs text-neutral-600">
+                Рейтингове оцінювання професійної діяльності науково-педагогічних працівників є складовою
+                внутрішньої системи забезпечення якості вищої освіти та освітньої діяльності університету.
+            </p>
+
+            <div className="w-full overflow-x-scroll">
+                <table className="-ml-6 -mr-6 border-separate whitespace-nowrap border-spacing-x-6 border-spacing-y-2">
+                    <thead className="text-sm text-left text-primary">
+                        <tr className="border-2 border-solid border-neutral-950">
+                            <th>Навчальний рік</th>
+                            <th>Рейтинг з навчально-методичної роботи</th>
+                            <th>Рейтинг з науково-інноваційної роботи</th>
+                            <th>Рейтинг з організаційно-виховної роботи</th>
+                            <th>Загальний рейтинг</th>
+                            <th>Кафедра</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="text-xs text-neutral-600">
+                        {ratings.map(rating => (
+                            <tr key={rating.studyYear}>
+                                <td>{rating.studyYear}</td>
+                                <td>{rating.educationalMethodologicalRating}</td>
+                                <td>{rating.scientificInnovativeRating}</td>
+                                <td>{rating.organizationalEducationalRating}</td>
+                                <td>{rating.overallRating}</td>
+                                <td>{rating.subdivision.name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
