708702c1a7252d5c59eb29ca8aa0a0e9cc0ebab0
Markusplay <mshilo2004@gmail.com>
2025-01-14 18:08:41 +0200

    remove unused getByQueryString

--- src/api/subdivision.ts
+++ src/api/subdivision.ts
@@ -4,8 +4,3 @@ export const getFaculties = (): Promise<{ name: string }[]> => {
     return Http.get(API_BASE_URL + '/subdivision/faculty/')
         .then((response: any) => response.map((faculty: { name: string; }) => faculty.name));
 };
-
-export const getByQueryString = (q: string): Promise<string[]> => {
-    return Http.get(API_BASE_URL + '/subdivision/all?q=' + q)
-        .then((response: any) => response.map((department: { name: string; }) => department.name));
-};
