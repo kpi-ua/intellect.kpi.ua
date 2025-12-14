6b8d45d2358f034ef44b32c0594b6c8931878e69
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-12-14 18:23:38 +0200

    Fix image error

--- src/components/InfoBlock/InfoBlock.tsx
+++ src/components/InfoBlock/InfoBlock.tsx
@@ -10,7 +10,7 @@ const InfoBlock: React.FC<Props> = ({ sectionImg, children }) => {
     return (
         <section className="flex justify-between items-start mt-9 mb-140 gap-4">
             <div className="max-w-500 shrink-[2]">{children}</div>
-            <Image className="max-w-480 hidden sm:block h-auto w-full shrink" src={sectionImg} alt="sectionImg" />
+            <Image className="max-w-480 hidden sm:block h-auto w-full shrink" src={sectionImg} width={480} height={480}  alt="sectionImg" />
         </section>
     );
 };
